28c422001b6e6debd9ff6f74a61257df7274d0fa
borsuk7 <petro.solomchak.7@gmail.com>
2023-11-13 14:53:08 +0200

    =Test

--- BuildExample/Calculator.cs
+++ BuildExample/Calculator.cs
@@ -3,7 +3,7 @@
 public static class Calculator
 {
     public static double Add(double a, double b) => a + b;
-    public static double Subtract(double a, double b) => a - b;
+    public static double Subtract(double a, double b) => a + b;
     public static double Multiply(double a, double b) => a * b;
 
     public static double Divide(double a, double b)
